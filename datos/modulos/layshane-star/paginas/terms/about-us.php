05dd13620ef5f29111fe4c091dc1873200fee588
Luis lopez estela <jose_1.9.96@hotmail.com>
2023-10-07 08:21:34 -0500

    tems

--- datos/modulos/layshane-star/paginas/terms/about-us.php
+++ datos/modulos/layshane-star/paginas/terms/about-us.php
@@ -1,16 +1,4 @@
-<div class="">
-	<div class=""> 
-		<div class="container"> 
-			<div>
-				<h1 class="text-center"> 
-					<?php echo $wo['lang']['about'];?><br> 
-					<small class="site_desc text-center">Bienvenidos a Layshane Perú</small> 
-				</h1>
-			</div> 
-		</div> 
-	</div>
-</div>
-<hr>
+
 <div class="page-margin">
    <div class="wow_content">
       <div class="setting-well" style="text-align:center;">
